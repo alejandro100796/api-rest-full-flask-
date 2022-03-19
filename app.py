b71ca7a8c8b2fb21cf1e089185077db36414e57d
ENRIQUE ALEJANDRO VASQUEZ VASQUEZ <evasquezv@ad.elektra.com.mx>
2022-03-18 20:35:29 -0600

    CORS

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from datetime import datetime
 from database import products, setup
 
 app = Flask(__name__)
+CORS(app)
 
 setup.create_tables()
 
